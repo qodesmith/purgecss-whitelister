d7c05c2f5151d0aae5c871dfc58182a3bc4dc489
Aaron Cordova <theqodesmith@gmail.com>
2017-12-30 14:06:52 -0500

    Initial commit!

---
+++ index.js
@@ -0,0 +1,64 @@
+const listSelectors = require('list-css-selectors');
+const cssWhat = require('css-what');
+const glob = require('glob');
+
+function makeWhitelist(filenames) {
+  if (!Array.isArray(filenames)) filenames = [filenames];
+  if (!filenames.length) throw 'You gave me an empty array. I feel so empty inside...';
+  if (filenames.some(name => !name.split)) throw `Oops! Something passed wasn't a string.`;
+
+  // If globs were passed, boil everything down to a flat array of absolute paths.
+  filenames = flattenArray(filenames.map(name => glob.sync(name)));
+  if (!filenames.length) {
+    console.log('\n\nNo matching files found for whitelisting.');
+    console.log('Proceeding with an empty array.\n\n');
+    return [];
+  }
+
+  const selectorErrors = [];
+  const selectors = listSelectors(filenames);
+  const whitelist = selectors.map(selector => {
+    let name;
+
+    try {
+      const what = cssWhat(selector);
+      name = what[0].map(obj => {
+        if (obj.type.includes('pseudo')) return;
+        if (obj.type === 'tag') return obj.name;
+        return obj.value || obj.name;
+      });
+    } catch (e) {
+      selectorErrors.push(selector);
+    }
+
+    return name && name.filter(Boolean);
+  }).filter(Boolean);
+
+  if (selectorErrors.length) {
+    console.log('\n\nErrors with the following selectors:');
+    selectorErrors.forEach(selector => console.log(`  ${selector}`));
+    console.log('\n\n');
+  }
+
+  const flatWhitelist = flattenArray(whitelist);
+  return Array.from(new Set(flatWhitelist)); // Remove duplicates.
+}
+
+function flattenArray(arr) {
+  const isArray = Array.isArray(arr);
+  if (!isArray) return arr;
+
+  let finalArray = [];
+
+  arr.forEach(thing => {
+    if (isArray) {
+      finalArray = finalArray.concat(flattenArray(thing));
+    } else {
+      finalArray.push(thing);
+    }
+  });
+
+  return finalArray;
+}
+
+module.exports = makeWhitelist;
